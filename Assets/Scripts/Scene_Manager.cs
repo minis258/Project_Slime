779c510dd2683d8ddc886b114c79b46a2141642f
minis258 <minhtri.pn@hotmail.de>
2021-04-13 12:27:15 +0200

    Scene-, SoundManager added
    Scence change with strings
    Sound script added with random free copyright sound and slider to change volume

---
+++ Assets/Scripts/Scene_Manager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    public void LoadScenes(string _scene)
+    {
+        SceneManager.LoadScene(_scene);
+    }
+
+
+}
---
+++ Assets/Scripts/Sound_Manager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Manager : MonoBehaviour
+{
+    private AudioSource p_AudioSource;
+
+    private float p_Volume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p_AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckVolume();
+    }
+
+    private void CheckVolume()
+    {
+        p_AudioSource.volume = p_Volume;
+    }
+
+    public void SetVolume(float _vol)
+    {
+        p_Volume = _vol;
+    }
+}
